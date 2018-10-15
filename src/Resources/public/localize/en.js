36405af1d0704889a75a20f4c87e31b4f45c1b80
Bilal YILMAZ <mail@bilalyilmaz.com>
2018-10-15 14:03:31 +0300

    Dil dosyası düzenlendi

--- src/Resources/public/localize/en.js
+++
@@ -1,5 +0,0 @@
-$.nmlb.langs.en = {
-	main_title		: "Media Library",
-	upload_title	: "File Upload",
-}
-$.nmlb.lang = "en";
---
+++ src/Resources/public/localize/tr.js
@@ -0,0 +1,16 @@
+$.nmlb.langs.tr = {
+	main_title		: "Ortam Ekle",
+	upload_title	: "Dosya Yükle",
+	explore_title	: "Ortam Kütüphanesi",
+	search_input_ph	: "Ortam dosyalarında ara...",
+	selected		: "seçildi",
+	clear			: "Temizle",
+	upload_area_msg	: "Yüklemek için dosyaları sürükleyip bırakın",
+	not_found		: "Hiç bir öğe bulunamadı",
+	upload_msg_l1	: "Yüklemek istediğiniz dosyaları herhangi bir yere sürükleyip bırakın",
+	upload_msg_l2	: "ya da",
+	upload_msg_btn	: "Dosya Seçin",
+	upload_msg_l3	: "Yüklenebilecek en büyük dosya boyutu: 2 MB.",
+	add				: "Ekle",
+}
+$.nmlb.lang = "tr";
